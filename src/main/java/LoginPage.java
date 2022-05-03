757970eb5da2b43cec2599a5a9e0b383e7f8721f
YasinHuseyn <forego687@gmail.com>
2022-05-04 00:04:35 +0400

    Login

---
+++ src/main/java/LoginPage.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginPage extends JFrame {
+    private JTextField userText;
+    private JPanel panel1;
+    private JPasswordField password;
+    private JButton loginButton;
+    private JFrame frame;
+
+    public LoginPage(){
+        frame=new JFrame("Login Frame");
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(250,200));
+        frame.setResizable(false);
+
+        frame.add(panel1);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+
+    }
+
+    public static class Main {
+    }
+}
+
